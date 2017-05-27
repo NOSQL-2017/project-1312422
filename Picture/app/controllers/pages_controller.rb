cf7c20a3af74cfecf9958b50de70b05b0210446c
DUYPHAT\Phat <1312422@student.hcmus.edu.vn>
2017-05-27 16:42:07 +0700

    wall images

--- Picture/app/controllers/pages_controller.rb
+++ Picture/app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
+require'pry'
 class PagesController < ApplicationController
 	def home
+		binding.pry
+		@images = Image.joins("INNER JOIN friends ON (friends.friend_id = images.user_id AND friends.user_id = #{session[:current_user_id]}) OR (images.user_id = #{session[:current_user_id]})").order('images.created_at desc').distinct.paginate(:page => params[:page], :per_page => 8)
 	end
 
 	def about
