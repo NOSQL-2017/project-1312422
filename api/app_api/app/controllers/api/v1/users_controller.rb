997177801b6d0088e08c5ebd85ceeaacc1bb4445
DUYPHAT\Phat <1312422@student.hcmus.edu.vn>
2017-06-18 01:23:16 +0700

    login

--- api/app_api/app/controllers/api/v1/users_controller.rb
+++ api/app_api/app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,21 @@
-class Api::V1::ArticlesController < ApplicationController
+class Api::V1::UsersController < ApplicationController
   def index
   	@users = User.all
   	render json: @users
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	@user.token = SecureRandom.uuid.gsub(/\-/,'')
+  	if @user.save
+        render json: { token: user.token }, status: :ok
+    else
+        render json: { errors: 'Wrong email or password' }, status: :unauthorized
+    end
+  end
+
+  private
+  def user_params
+      params.require(:user).permit(:username, :password, :name, :avatar, :gender, :email)
   end
 end
--- api/app_api/db/schema.rb
+++ api/app_api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170529080927) do
+ActiveRecord::Schema.define(version: 20170617175858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 20170529080927) do
     t.string "email"
     t.string "provider"
     t.string "uid"
+    t.string "token"
   end
 
   add_foreign_key "friends", "users"
